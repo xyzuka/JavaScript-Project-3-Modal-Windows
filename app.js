148ff7b3f80f8f3619695f2b372a4c9dd496dde5
xyzuka <94155478+xyzuka@users.noreply.github.com>
2022-01-21 23:23:10 +1100

    Shortening DOM Manipulation elements into functions for code readability

--- app.js
+++ app.js
@@ -6,19 +6,27 @@ const modal = document.querySelector(".modal");
 const close_modal = document.querySelector(".close-modal");
 const overlay = document.querySelector(".overlay");
 
+//Function that opens the modal
+const openModal = function() {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden'); //adds background blur to modal pop up by removing the hidden class that hides the blur
+} 
+    //element.classList - returns a DOMTokenList which can be later modified. Eg above: we are removing the hidden class from the element modal
+
+//Function that closes the modal
+const closeModal = function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
 //Loop to respond to a click event
 for (let i = 0; i < show_modal.length; i++)
     // console.log(show_modal[i].textContent); //node.textContent - returns the text content of a specified node ie. show_modal 
-    show_modal[i].addEventListener('click', function() {
-        console.log("Button clicked");
-        modal.classList.remove('hidden');
-        overlay.classList.remove('hidden'); //adds background blur to modal pop up by removing the hidden class that hides the blur
-    }); 
+    show_modal[i].addEventListener('click', openModal);
     //.addEventListener() - sets up a function that will be run whenever a specified event is delivered ie. a click in this case
-    //element.classList - returns a DOMTokenList which can be later modified. Eg above: we are removing the hidden class from the element modal
 
 //Close button functionality
-close_modal.addEventListener('click', function() {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-})
+close_modal.addEventListener('click', closeModal);
+
+//Closes modal window when clicking outside
+overlay.addEventListener('click', closeModal);
